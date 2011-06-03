1b5a4bea1f0feec6eb191e7d44889c63652cc3ea
Andrey Raykov <sergei.struck@gmail.com>
2011-06-03 16:52:08 +0300

    change solo.rb dir ways

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
-file_cache_path "~/vagrant_app1"
-cookbook_path "~/vagrant_app1/cookbooks"
-role_path "~/vagrant_app1/roles"
-json_attribs "~/vagrant_app1/node.json"
+file_cache_path "/tmp/vagrant_app1"
+cookbook_path "/tmp/vagrant_app1/cookbooks"
+role_path "/tmp/vagrant_app1/roles"
+json_attribs "/tmp/vagrant_app1/node.json"
 
