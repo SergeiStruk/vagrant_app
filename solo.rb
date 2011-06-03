7aa91e0f473ec336862ed34a7f78462e2fd0b203
Andrey Raykov <sergei.struck@gmail.com>
2011-06-03 16:27:36 +0300

    fix solo.rb

--- solo.rb
+++ solo.rb
@@ -1,4 +1,5 @@
-file_cache_path "~/vagrant_app1/"
+file_cache_path "~/vagrant_app1"
 cookbook_path "~/vagrant_app1/cookbooks"
-json_attribs "~/vagrant_app1/node.json"
 role_path "~/vagrant_app1/roles"
+json_attribs "~/vagrant_app1/node.json"
+
