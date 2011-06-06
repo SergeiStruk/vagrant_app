0353174e50bcbe04003aebaf630968199e75c604
Andrey Raykov <sergei.struck@gmail.com>
2011-06-06 12:06:19 +0300

    change install sh

--- insall.sh
+++ insall.sh
@@ -1,1 +1,12 @@
-sudo apt-get update && sudo apt-get install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert git-core -y && sudo apt-get install rubygems1.9.1  && sudo gem install chef && cd /tmp/ && git clone git://github.com/SergeiStruk/vagrant_app1.git && sudo chef-solo -c /tmp/vagrant_app1/solo.rb -j /tmp/vagrant_app1/node.json
+sudo apt-get update 
+sudo apt-get install ruby wget ssl-cert git-core -y
+mkdir rubygems
+pushd rubygems
+sudo apt-get install subversion -y
+svn checkout svn://rubyforge.org/var/svn/rubygems/trunk
+cd trunk
+sudo ruby setup.rb
+sudo gem install chef
+cd /tmp/ 
+git clone git://github.com/SergeiStruk/vagrant_app1.git  
+sudo chef-solo -c /tmp/vagrant_app1/solo.rb -j /tmp/vagrant_app1/node.json
