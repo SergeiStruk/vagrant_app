efa2deed4e6ca328e266a71ff378ef3cdbf6d8c0
Andrey Raykov <sergei.struck@gmail.com>
2011-06-06 14:53:49 +0300

    change ruby recipe and install.sh

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -22,7 +22,7 @@
 
 #exec "sudo apt-get install git-core -y && sudo apt-get -y install curl && sudo curl https://github.com/joshfng/railsready/raw/master/railsready.sh >> railsready.sh && sudo chmod a+x railsready.sh && sudo ./railsready.sh -y"
 
-ruby = 'sudo apt-get install build-essential bison openssl libreadline5 libreadline5-dev curl zlib1g zlib1g-dev libssl-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libmysqlclient-dev -y && mkdir -p ~/temp/ruby && cd ~/temp/ruby && wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz && tar xzf ruby-1.9.2-p180.tar.gz && cd ruby-1.9.2-p180 && ./configure --enable-pthread --with-ext=bigdecimal,continuation,coverage,curses,dbm,digest,digest/bubblebabble,digest/md5,digest/rmd160,digest/sha1,digest/sha2,dl,etc,fcntl,fiber,fiddle,gdbm,iconv,io,io/nonblock,json,json/generator,json/parser,mathn,mathn/complex,mathn/rational,nkf,nkf/nkf-utf8,objspace,openssl,psych,pty,readline,ripper,sdbm,socket,stringio,strscan,syck,syslog,zlib && make && make install &&sudo gem install fpm && mkdir /tmp/installdir && make install DESTDIR=/tmp/installdir && fpm -s dir -t deb -n ruby1.9.1p180 -v 1.9.2p180 -C /tmp/installdir -p ruby-VERSION_ARCH.deb -d "libssl0.9.8 (>= 0.9.8k-1)" -d "libc6 (>= 2.2.5)" -d "libgdbm3 (>= 1.8.3)" -d "libncurses5 (>= 5.6+20071006-3)" -d "libreadline5 (>= 5.2)" -d "zlib1g (>= 1:1.1.4)" -d "libc6-dev (>= 0)" -d "build-essential (>= 0)"'
+#ruby = 'sudo apt-get install build-essential bison openssl libreadline5 libreadline5-dev curl zlib1g zlib1g-dev libssl-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libmysqlclient-dev -y && mkdir -p ~/temp/ruby && cd ~/temp/ruby && wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz && tar xzf ruby-1.9.2-p180.tar.gz && cd ruby-1.9.2-p180 && ./configure --enable-pthread --with-ext=bigdecimal,continuation,coverage,curses,dbm,digest,digest/bubblebabble,digest/md5,digest/rmd160,digest/sha1,digest/sha2,dl,etc,fcntl,fiber,fiddle,gdbm,iconv,io,io/nonblock,json,json/generator,json/parser,mathn,mathn/complex,mathn/rational,nkf,nkf/nkf-utf8,objspace,openssl,psych,pty,readline,ripper,sdbm,socket,stringio,strscan,syck,syslog,zlib && make && make install &&sudo gem install fpm && mkdir /tmp/installdir && make install DESTDIR=/tmp/installdir && fpm -s dir -t deb -n ruby1.9.1p180 -v 1.9.2p180 -C /tmp/installdir -p ruby-VERSION_ARCH.deb -d "libssl0.9.8 (>= 0.9.8k-1)" -d "libc6 (>= 2.2.5)" -d "libgdbm3 (>= 1.8.3)" -d "libncurses5 (>= 5.6+20071006-3)" -d "libreadline5 (>= 5.2)" -d "zlib1g (>= 1:1.1.4)" -d "libc6-dev (>= 0)" -d "build-essential (>= 0)"'
 app_ssh = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwu0Mj/YqFDM88UwvTMbEIghiunkEScoMJZ+TDhg+VOI79OY4/+nyNUkbeH/52SFA6apywvJlN6RIkuKpGK+7idjyBT4L17yiS/ZZGvTXJjol04m814TMJveOwftXN7/eb0zUl6nsEJMTOU8EPMd9KSDiVIGki3REOGWO+Ju/Cij163kqvaNQk8SI2m4sAni+RxILsFTrvBdzuzBsyFji0dlVotuXkigYSZry5wfqEVcFaSCA4zGvOby+IPhBCk8YQw94bG+Ta+Ra78mmoysIc5QEP/sVzWk5/FjIGZJxl3SL3Q4jcXXbfjqhRZHPc9NGPdhud8Dla5QwmF0OCuodYw== sst@jetthoughts.com'
 add_ssh = 'sudo echo '+ app_ssh + '>> ~/.ssh/authorized_keys '
 result = ' ' 
@@ -31,9 +31,11 @@ if (%x(cat ~/.ssh/authorized_keys).include? app_ssh ) == false
   (result == ' ') ? (result += add_ssh) : (result += " && " + add_ssh)
 end
 
+=begin
 if (%x(ruby -v).include? 'ruby 1.9.2p180') == false
   (result == ' ') ? (result += ruby) : (result += " && " + ruby)
 end
+=end
 
 =begin
 if (%x(rails -v).include? 'Rails 3.0.7') == false
