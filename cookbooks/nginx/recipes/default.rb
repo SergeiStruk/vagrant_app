b18c730673809cfc459d0f6f4834555879d770ec
Andrey Raykov <sergei.struck@gmail.com>
2011-06-09 19:00:06 +0300

    add nginx conf filed

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,24 +1,16 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-# Author:: AJ Christensen <aj@junglist.gen.nz>
-#
-# Copyright 2008-2009, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+nginx = ['sudo apt-get install nginx -y',
+  'cd /etc/ ',
+  'mkdir nginx',
+  '/tmp/vag',
+  'mv  /tmp/vagrant_app1/cookbooks/nginx/nginx.conf /etc/nginx/nginx.conf'
+  ]
+  
+nginx.each{|x| system(x) }
 
+=begin
 package "nginx"
+#nginx='sudo apt-get install nginx -y'
+#system(nginx)
 
 directory node[:nginx][:log_dir] do
   mode 0755
@@ -54,3 +46,4 @@ service "nginx" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
+=end
