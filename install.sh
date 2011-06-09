0182abb082e26d93b4b0cff41f3e1290352fb7e4
Andrey Raykov <sergei.struck@gmail.com>
2011-06-09 15:06:57 +0300

    change ruby install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get install curl wget ssl-cert git-core -y
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source .bash_profile
-/usr/bin/apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev -y
+sudo /usr/bin/apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev -y
 rvm install 1.9.2
 rvm use 1.9.2
 rvm use 1.9.2 --default
