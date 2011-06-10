c087334ea9a3690d78db6e6679d59500a314ab26
Andrey Raykov <sergei.struck@gmail.com>
2011-06-10 15:12:46 +0300

    add nginx recipe

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,4 +1,25 @@
+  #
+# Cookbook Name:: nginx
+# Recipe:: default
+# Author:: AJ Christensen <aj@junglist.gen.nz>
+#
+# Copyright 2008-2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 package "nginx"
+
 
 directory node[:nginx][:log_dir] do
   mode 0755
@@ -22,6 +43,7 @@ template "nginx.conf" do
   group "root"
   mode 0644
 end
+
 =begin
 template "#{node[:nginx][:dir]}/sites-available/default" do
   source "default-site.erb"
@@ -30,6 +52,7 @@ template "#{node[:nginx][:dir]}/sites-available/default" do
   mode 0644
 end
 =end
+
 service "nginx" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
