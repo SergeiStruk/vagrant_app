af62efe4bc8e214eccb57653754bcf7aa0a18e9f
Andrey Raykov <sergei.struck@gmail.com>
2011-06-10 11:30:59 +0300

    change nginx install

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,24 +1,18 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-# Author:: AJ Christensen <aj@junglist.gen.nz>
-#
-# Copyright 2008-2009, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+=begin
+nginx = ['sudo apt-get install nginx -y',
+  'sudo mv  /tmp/vagrant_app1/cookbooks/nginx/nginx.conf /etc/nginx/',
+  'sudo mv  /tmp/vagrant_app1/cookbooks/nginx/nxdissite /usr/sbin/',
+  'sudo mv  /tmp/vagrant_app1/cookbooks/nginx/nxensite /usr/sbin/',
+  'sudo mv  /tmp/vagrant_app1/cookbooks/nginx/default /etc/nginx/sites-available/'
+  ]
+
+nginx.each{|x| system(x) }
+=end
+
 
 package "nginx"
+#nginx='sudo apt-get install nginx -y'
+#system(nginx)
 
 directory node[:nginx][:log_dir] do
   mode 0755
@@ -42,15 +36,16 @@ template "nginx.conf" do
   group "root"
   mode 0644
 end
-
+=begin
 template "#{node[:nginx][:dir]}/sites-available/default" do
   source "default-site.erb"
   owner "root"
   group "root"
   mode 0644
 end
-
+=end
 service "nginx" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
+
