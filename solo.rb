fad174e6179e5fda4f55635daf35d78f29d6110b
Andrey Raykov <sergei.struck@gmail.com>
2011-06-20 21:07:40 +0300

    chnge solo.rb

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
-file_cache_path "/tmp/vagrant_app1"
-cookbook_path "/tmp/vagrant_app1/cookbooks"
-role_path "/tmp/vagrant_app1/roles"
-json_attribs "/tmp/vagrant_app1/node.json"
+file_cache_path "/var/www/vagrant_app1"
+cookbook_path "/var/www/vagrant_app1/cookbooks"
+role_path "/var/www/vagrant_app1/roles"
+json_attribs "/var/www/vagrant_app1/node.json"
 
